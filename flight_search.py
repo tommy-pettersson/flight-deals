914897719bd24399096fd1e98c1620cf9636d215
Tommy Pettersson <tommypettersson04@gmail.com>
2022-10-19 14:05:34 +0200

    complete get_iata_code()

--- flight_search.py
+++ flight_search.py
@@ -1,3 +1,29 @@
+import requests
+
+API_KEY = "65MPkwtrYQKRmCHU_75lyaDUPkUJVn3w"
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+    
+    def get_iata_code(city_name):
+
+        endpoint = "https://api.tequila.kiwi.com/locations/query"
+
+        header = {
+            "apikey": API_KEY,
+        }
+
+        parameters = {
+            "term": city_name
+        }
+
+        try:
+            response = requests.get(url=endpoint, params=parameters, headers=header)
+            response.raise_for_status()
+        except requests.HTTPError as e:
+            print(e)
+        else:
+            data = response.json()
+            first_city = data["locations"][0]
+            city_code = first_city["code"]
+            return city_code
+
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from data_manager import DataManager
+from flight_search import FlightSearch
 from pprint import pprint
 
 def main():
-
-    sheet_data = DataManager.get_sheet_data()
-    pprint(sheet_data)
+    
+    location_data = FlightSearch.get_iata_code("stockholm")
+    pprint(location_data)
 
 
 if __name__ == "__main__":
